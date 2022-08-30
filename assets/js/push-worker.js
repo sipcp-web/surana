02576ed76d9d963d79a3e79dd5e4db56443fd1e7
Surana ind pu college -peenya <111618954+sipcp-web@users.noreply.github.com>
2022-08-30 12:31:09 +0530

    Add files via upload

---
+++ assets/js/push-worker.js
@@ -0,0 +1,6 @@
+self[`appKey`] = `01be8281d3507bc7618d64000590df4f`;
+self[`hostUrl`] = `https://cdn.gravitec.net/sw`;
+self.importScripts(`${self[`hostUrl`]}/worker.js`);
+// uncomment and set path to your service worker
+// if you have one with precaching functionality (has oninstall, onactivate event listeners)
+// self.importScripts('path-to-your-sw-with-precaching')
